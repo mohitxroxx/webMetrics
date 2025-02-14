e406e92f79a38b862f75121bc0b7c687e813bf1f
Mohitxroxx <mohitsinghroxx@gmail.com>
2025-02-14 14:45:16 +0530

    Completed server setup

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("HEYYYY")
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	fmt.Println("Server is up and running at http://localhost:5000")
+	log.Fatal(http.ListenAndServe("localhost:5000", r))
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1> Server is up and Running<h1>"))
 }
