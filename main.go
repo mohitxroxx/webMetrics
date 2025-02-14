aaef4b93dec41b5a0fd896d782b5b7151037e804
Mohitxroxx <mohitsinghroxx@gmail.com>
2025-02-14 17:07:41 +0530

    recording metrics for each route

--- main.go
+++ main.go
@@ -1,20 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"sync"
+	"time"
 )
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", serveHome).Methods("GET")
-	fmt.Println("Server is up and running at http://localhost:5000")
-	log.Fatal(http.ListenAndServe("localhost:5000", r))
+type Metrics struct {
+	RequestCount int            `json:"request_count"`
+	Endpoints    map[string]int `json:"endpoints"`
+	mu           sync.Mutex
 }
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1> Server is up and Running<h1>"))
+func NewMetrics() *Metrics {
+	return &Metrics{
+		Endpoints: make(map[string]int),
+	}
+}
+
+func (m *Metrics) increment(path string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.RequestCount++
+	m.Endpoints[path]++
+}
+
+func main() {
+	metrics := NewMetrics()
+	setupRoutes(metrics)
+}
+
+func setupRoutes(metrics *Metrics) {
+
+	withMetrics := func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			metrics.increment(r.URL.Path)
+			next(w, r)
+		}
+	}
+
+	http.HandleFunc("/", withMetrics(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Millisecond * 100)
+		w.Write([]byte("Hello, World!"))
+	}))
+
+	http.HandleFunc("/metrics", withMetrics(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(metrics)
+	}))
+
+	fmt.Println("Server is up and running at http://localhost:5000")
+
+	log.Fatal(http.ListenAndServe("localhost:5000",nil))
 }
